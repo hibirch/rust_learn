a5cce46098e550697ccd8fdb3ee02a233e26f52b
hibirch <jiyou1726@163.com>
2022-10-21 18:32:58 +0800

    Done

---
+++ enum_example/src/main.rs
@@ -0,0 +1,8 @@
+enum IpAddKind {
+    v4,
+    v6,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -1,6 +1,14 @@
+#[derive(Debug)]
+
 struct Rectangle {
     width: u32,
     length: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.length
+    }
 }
 
 fn main() {
@@ -9,9 +17,7 @@ fn main() {
         length: 50,
     };
 
-    println!("{}", area(&rect))
-}
+    println!("{}", rect.area());
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.length
+    println!("{:#?}", rect);
 }
